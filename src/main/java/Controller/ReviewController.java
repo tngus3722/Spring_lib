e0921c4dced612f5fc967f212cf133ca40a940e1
Administrator <tngus3722@koreatech.ac.kr>
2020-07-25 02:25:39 +0900

    0725

--- src/main/java/Controller/ReviewController.java
+++ src/main/java/Controller/ReviewController.java
@@ -7,9 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.util.List;
 
 @Controller
 public class ReviewController {
